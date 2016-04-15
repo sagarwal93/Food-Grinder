35eb12cb727fd78616ea7968146bf7f456ebf9a3
Samarth <samarth.agarwal1993@gmail.com>
2016-04-15 14:28:31 -0400

    Routing from Orders page

--- Client/src/containers/Orders.jsx
+++ Client/src/containers/Orders.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import OrderCurrent from './../components/OrderCurrent'; //eslint-disable-line
 import OrderList from './../components/OrderList'; //eslint-disable-line
 import * as actionCreators from '../action_creators';
+import Dialog from 'material-ui/lib/dialog'; //eslint-disable-line
+import {Link} from 'react-router'; //eslint-disable-line
 
 export const Orders = React.createClass({
+
+  getInitialState() {
+    return {
+      orderCardOpen: true
+    };
+  },
+
+  showNextOrder() {
+    this.props.nextOrder();
+  },
+
   render () {
     return (
       <div>
@@ -20,17 +32,20 @@ export const Orders = React.createClass({
               Welcome, {this.props.customer.get('name')}
             </div>
           }
-          <button style={{
-            padding: '2%',
-            backgroundColor: '#FF5722',
-            color: '#fff',
-            border: '0px',
-            fontSize: '16px',
-            width: '50%',
-            margin: '1% 0 5% 0'
-          }}>
-            Browse Orders
-          </button>
+          <Link to='order'>
+            <button style={{
+              padding: '2%',
+              backgroundColor: '#FF5722',
+              color: '#fff',
+              border: '0px',
+              fontSize: '16px',
+              width: '50%',
+              margin: '1% 0 5% 0'
+            }}
+            onClick={this.showNextOrder}>
+              Browse Orders
+            </button>
+          </Link>
         </div>
         {
           this.props.orders &&
@@ -54,7 +69,8 @@ export const Orders = React.createClass({
 function mapStateToProps(state) {
   return {
     customer: state.get('customer'),
-    orders: state.getIn(['customer', 'favoriteOrders'])
+    orders: state.getIn(['customer', 'favoriteOrders']),
+    currentOrder: state.get('currentOrder')
   };
 }
 
