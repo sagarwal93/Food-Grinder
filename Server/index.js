adb300e6e85434c94467e99e317a22c9a05bcbf1
Samarth <samarth.agarwal1993@gmail.com>
2016-04-15 09:49:23 -0400

    Removed es to make things work when youre off the NCR network

--- Server/index.js
+++ Server/index.js
@@ -1,26 +1,32 @@
 import makeStore from './src/store';
 import {startServer} from './src/server';
-import {esclient, buildChewsierOrders} from './src/esclient';
+// import {esclient, buildChewsierOrders} from './src/esclient';
+
 export const store = makeStore();
 startServer(store);
 
 store.dispatch({
-  type: 'SET_CUSTOMERS',
-  customers: require('./customers.json')
+  type: 'SET_ORDERS',
+  orders: require('./orders.json')
 });
 
-esclient.search({
-  index: 'chewsier',
-  type: 'orders'
-}).then(
-  function(resp) {
-    let esorders = resp.hits.hits;
-    store.dispatch({
-      type: 'SET_ORDERS',
-      orders: buildChewsierOrders(esorders)
-    });
-  },
-  function(err) {
-    console.log(err); //eslint-disable-line
-  }
-);
+store.dispatch({
+  type: 'SET_CUSTOMER',
+  customer: require('./customers.json')
+});
+
+// esclient.search({
+//   index: 'chewsier',
+//   type: 'orders'
+// }).then(
+//   function(resp) {
+//     let esorders = resp.hits.hits;
+//     store.dispatch({
+//       type: 'SET_ORDERS',
+//       orders: buildChewsierOrders(esorders)
+//     });
+//   },
+//   function(err) {
+//     console.log(err); //eslint-disable-line
+//   }
+// );
