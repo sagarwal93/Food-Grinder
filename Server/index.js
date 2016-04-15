20289847b5338159339ef55f6db7c53aa27c402e
ws185067 <ws185067@corp.ncr.com>
2016-04-15 09:20:03 -0400

    Switched back to getting orders from JOSN file.

--- Server/index.js
+++ Server/index.js
@@ -1,26 +1,31 @@
 import makeStore from './src/store';
 import {startServer} from './src/server';
-import {esclient, buildChewsierOrders} from './src/esclient';
+import {esclient, buildChewsierOrders} from './src/esclient'; //eslint-disable-line
 export const store = makeStore();
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ORDERS',
+  orders: require('./orders.json')
+});
 
 store.dispatch({
   type: 'SET_CUSTOMERS',
   customers: require('./customers.json')
 });
 
-esclient.search({
-  index: 'chewsier',
-  type: 'orders'
-}).then(
-  function(resp) {
-    let esorders = resp.hits.hits;
-    store.dispatch({
-      type: 'SET_ORDERS',
-      orders: buildChewsierOrders(esorders)
-    });
-  },
-  function(err) {
-    console.log(err); //eslint-disable-line
-  }
-);
+// esclient.search({
+//   index: 'chewsier',
+//   type: 'orders'
+// }).then(
+//   function(resp) {
+//     let esorders = resp.hits.hits;
+//     store.dispatch({
+//       type: 'SET_ORDERS',
+//       orders: buildChewsierOrders(esorders)
+//     });
+//   },
+//   function(err) {
+//     console.log(err); //eslint-disable-line
+//   }
+// );
